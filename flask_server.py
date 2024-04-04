6682b82b97277c4594ef739fae74a3bb9ba9de36
Ayfred <maxime.mu@mines-ales.org>
2024-04-04 14:22:55 +0100

    fix bugs

--- flask_server.py
+++ flask_server.py
@@ -11,20 +11,31 @@ app = Flask(__name__)
 @app.route('/get_data', methods=['GET'])
 def get_data():
     with app.app_context():
-        db = firestore.Client()
-        documents = db.collection('document').get()
+        db = firestore.client()
+        documents = db.collection('documents').get()
         data = [doc.to_dict() for doc in documents]
         return jsonify(data)
+    
+@app.route('/get_users', methods=['GET'])
+def get_users():
+    with app.app_context():
+        db = firestore.client()
+        documents = db.collection('users').get()
+        data = [doc.to_dict() for doc in documents]
+        return jsonify(data)
+
+
+
 
 # Add data to database
 @app.route('/add_data', methods=['POST'])
 def add_data():
     with app.app_context():
-        db = firestore.Client()
+        db = firestore.client()
         data = request.json
-        db.collection('document').add(data)
+        db.collection('documents').add(data)
         return jsonify({"message": "success"})
 
 if __name__ == '__main__':
-    print(get_data())
     app.run(debug=True, host='localhost', port=5000)
+    print(get_data())
